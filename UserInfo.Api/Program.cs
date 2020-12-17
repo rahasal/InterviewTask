43203ab865fd2661b25dbe62e62f7bad2b2125e3
Raha.S <raha.salehpour@outlook.com>
2020-12-17 12:52:41 +0330

    change

--- UserInfo.Api/Program.cs
+++ UserInfo.Api/Program.cs
@@ -15,7 +15,7 @@ namespace UserInfo.Api
     {
         public static void Main(string[] args)
         {
-           CreateWebHostBuilder(args).Build().RunAsync();          
+           CreateWebHostBuilder(args).Build().Run();          
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
